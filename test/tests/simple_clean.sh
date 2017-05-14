4e9e3f7b7e6bd90a9c8ae06d468e41134349143b
Kevin Krummenauer <kevin@whiledo.de>
2017-05-15 01:22:54 +0200

    add new testcase

--- test/tests/simple_clean.sh
+++ test/tests/simple_clean.sh
@@ -10,17 +10,48 @@ setUp() {
 }
 
 
-testEquali() {
+testCleanASingleRepo() {
    startRegistry
+
+   #setup testdata
+   createTestdata "abc" 1 5  
+   assertImageExists "abc" 1
+   assertImageExists "abc" 2
+   assertImageExists "abc" 3
+   assertImageExists "abc" 4
+   assertImageExists "abc" 5
+
+   #run cleanreg 
+   runCleanreg -n abc -k 2
+   assertImageNotExists "abc" 1
+   assertImageNotExists "abc" 2
+   assertImageNotExists "abc" 3
+   assertImageExists "abc" 4
+   assertImageExists "abc" 5
+    
+}
+
+testCleanASingleRepoWithBasicAuthRegistry() {
+   startRegistryBasicAuth
+
+   #setup testdata
    createTestdata "abc" 1 5
-  
    assertImageExists "abc" 1
    assertImageExists "abc" 2
    assertImageExists "abc" 3
    assertImageExists "abc" 4
    assertImageExists "abc" 5
     
+   #Cleanreg without credentials -> no changes 
    runCleanreg -n abc -k 2
+   assertImageExists "abc" 1
+   assertImageExists "abc" 2
+   assertImageExists "abc" 3
+   assertImageExists "abc" 4
+   assertImageExists "abc" 5
+
+   #cleanreg with credentials should delete imagetags
+   runCleanreg -n abc -k 2 -u test -pw secret
 
    assertImageNotExists "abc" 1
    assertImageNotExists "abc" 2
@@ -28,7 +59,6 @@ testEquali() {
    assertImageExists "abc" 4
    assertImageExists "abc" 5
 
-  #runGarbageCollection
 }
 
 # load shunit2
