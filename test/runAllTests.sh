09d5265e4fd3a8e7e3f0f471d47a81963519cfb1
Kevin Krummenauer <kevin@whiledo.de>
2017-05-15 23:28:47 +0200

    tests are now executed inside a container

---
+++ test/runAllTests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#######################################
+#Runs a testfile within the testrunner container
+#Arguments:
+#  FILENAME the name of the testfile script inside the test/tests directory
+#######################################
+function runTestFile {
+   docker run --rm -it -v $(pwd):/data:ro -v /var/run/docker.sock:/var/run/docker.sock:ro testrunner "cd /data/test/tests; ./$1"
+}
+
+#Create the testrunner image
+docker build -t testrunner -f ./config/Dockerfile.testrunner ./config
+cd ..
+
+#Run the testfiles
+runTestFile simple_clean.sh
