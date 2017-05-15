e94d1eafa5a6415b1b35c2464e96995260d79adc
Kevin Krummenauer <kevin@whiledo.de>
2017-05-16 00:30:57 +0200

    move registry tag definition to test script

--- test/runAllTests.sh
+++ test/runAllTests.sh
@@ -4,9 +4,10 @@
 #Runs a testfile within the testrunner container
 #Arguments:
 #  FILENAME the name of the testfile script inside the test/tests directory
+#  REGISTRYTAG tag of the registry being used
 #######################################
 function runTestFile {
-   docker run --rm -it --net=host -v $(pwd):/data:ro -v /var/run/docker.sock:/var/run/docker.sock:ro testrunner "cd /data/test/tests; ./$1"
+   docker run --rm -it -e REGISTRYTAG=$2 --net=host -v $(pwd):/data:ro -v /var/run/docker.sock:/var/run/docker.sock:ro testrunner "cd /data/test/tests; ./$1"
 }
 
 #Create the testrunner image
@@ -14,4 +15,4 @@ docker build -t testrunner -f ./config/Dockerfile.testrunner ./config
 cd ..
 
 #Run the testfiles
-runTestFile simple_clean.sh
+runTestFile simple_clean.sh 2.5.1
