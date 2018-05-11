5353297b6cdc17e1485d2e976a52bbe1347b793c
Julian Sauer <julian_sauer@gmx.net>
2018-05-11 15:04:02 +0200

    Add tests for configs with regex and date

--- test/tests/cleanreg_config_check.sh
+++ test/tests/cleanreg_config_check.sh
@@ -7,7 +7,7 @@ oneTimeSetUp() {
    createRegistryImages
    createCleanregImage
    startRegistry
-   echo "abc 2" > $CLEANREG_WORKSPACE/test.conf
+   echo "abc 2 regex `date +%d.%m.%y`" > $CLEANREG_WORKSPACE/test.conf
 }
 
 oneTimeTearDown() {
@@ -73,6 +73,58 @@ test_either_f_n_or_cf_has_to_be_used() {
    lastLine=$(runCleanreg 2>&1 | tail -n 1 | tr -dC '[:print:]\t\n')
    echo $lastLine
    assertEquals "cleanreg.py: error: [-n|-k] or [-cf|-k] or [-f] has to be used!" "$lastLine"
+}
+
+test_f_with_k_works() {
+   createTestdata "abc" 1 5
+   assertImageExists "abc" 1
+   assertImageExists "abc" 2
+   assertImageExists "abc" 3
+   assertImageExists "abc" 4
+   assertImageExists "abc" 5
+
+   echo "abc 2 notmatching `date +%d.%m.`$((`date +%Y`+1))" > $CLEANREG_WORKSPACE/test.conf
+   runCleanregPython -f $CLEANREG_WORKSPACE/test.conf
+   assertImageNotExists "abc" 1
+   assertImageNotExists "abc" 2
+   assertImageNotExists "abc" 3
+   assertImageExists "abc" 4
+   assertImageExists "abc" 5
+}
+
+test_f_with_regex_works() {
+   createTestdata "abc" 1 5
+   assertImageExists "abc" 1
+   assertImageExists "abc" 2
+   assertImageExists "abc" 3
+   assertImageExists "abc" 4
+   assertImageExists "abc" 5
+
+   echo "abc 0 1|2 `date +%d.%m.`$((`date +%Y`+1))" > $CLEANREG_WORKSPACE/test.conf
+   cat $CLEANREG_WORKSPACE/test.conf
+   runCleanregPython -f $CLEANREG_WORKSPACE/test.conf
+   assertImageExists "abc" 1
+   assertImageExists "abc" 2
+   assertImageNotExists "abc" 3
+   assertImageNotExists "abc" 4
+   assertImageNotExists "abc" 5
+}
+
+test_f_with_date_works() {
+   createTestdata "abc" 1 5
+   assertImageExists "abc" 1
+   assertImageExists "abc" 2
+   assertImageExists "abc" 3
+   assertImageExists "abc" 4
+   assertImageExists "abc" 5
+
+   echo "abc 0 notmatching `date +%d.%m.%Y`" > $CLEANREG_WORKSPACE/test.conf
+   runCleanregPython -f $CLEANREG_WORKSPACE/test.conf
+   assertImageExists "abc" 1
+   assertImageExists "abc" 2
+   assertImageExists "abc" 3
+   assertImageExists "abc" 4
+   assertImageExists "abc" 5
 }
 
 # load shunit2
